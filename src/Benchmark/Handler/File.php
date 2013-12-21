fd9e8a886aaea313f7a7d20282a24cb4111834d9
CrazyCoder <true.isnt.false@gmail.com>
2013-12-21 16:20:12 +0700

    FileHandler implemented

--- src/Benchmark/Handler/File.php
+++ src/Benchmark/Handler/File.php
@@ -11,5 +11,40 @@ namespace Benchmark\Handler;
 
 class FileHandler implements HandlerInterface
 {
+    private $_resultCollection = [];
     
+    private $_outputFile;
+    
+    public function append (\Benchmark\ResultInterface $result)
+    {
+        $this->_resultCollection [] = $result;
+    }
+    
+    public function setOutputFile ($file)
+    {
+        if (is_readable ($file) && is_writeable ($file))
+        {
+            $this->_outputFile = $file;
+            return true;
+        }
+        
+        throw new \RuntimeException ();
+    }
+
+    public function persist ()
+    {
+        if (! $this->_outputFile)
+        {
+            throw new \BadMethodCallException ();
+        }
+        
+        $data = implode ('', array_map (function ($result) 
+        {
+           
+            return $result->report ();
+            
+        }, $this->_resultCollection));
+        
+        file_put_contents ($this->_outputFile, $data, FILE_APPEND);
+    }
 }
