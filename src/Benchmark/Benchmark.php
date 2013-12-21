a5f8347a7cd675e481c48a037f3fdee56688e134
crazy_coder <fromtmsk@gmail.com>
2013-12-21 16:22:20 +0400

    start for Benchmark class

--- src/Benchmark/Benchmark.php
+++ src/Benchmark/Benchmark.php
@@ -11,5 +11,25 @@ namespace Benchmark;
 
 class Benchmark
 {
+    private $_id, $_dataDir, $_resultObj, $_handlerStack;
     
+    public static function create ($id, $dataDir, $resultObj)
+    {
+        $instance = (new static ($id, $dataDir, $resultObj));
+        return $instance;
+    }
+    
+    public function __construct ($id, $dataDir, ResultInterface $resultObj)
+    {
+        $this->_handlerStack = new \SplStack ();
+        // just to make it obvious & easy to change
+        $this->_handlerStack->setIteratorMode (
+            \SplDoublyLinkedList::IT_MODE_FIFO
+                | \SplDoublyLinkedList::IT_MODE_KEEP
+        );
+        
+        $this->_id        = $id        ;
+        $this->_dataDir   = $dataDir   ;
+        $this->_resultObj = $resultObj ;
+    }
 }
