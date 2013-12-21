f780fe3f315cd2a705f8c0b7d4880a215bee1719
CrazyCoder <true.isnt.false@gmail.com>
2013-12-21 15:55:31 +0700

    Result implemented

--- src/Benchmark/Result.php
+++ src/Benchmark/Result.php
@@ -11,5 +11,25 @@ namespace Benchmark;
 
 class Result implements ResultInterface
 {
+    private $_id, $_usage;
     
+    public function getClone ()
+    {
+        return clone $this;
+    }
+
+    public function report ()
+    {
+        return "Started '{$this->_id}': took {$this->_usage} to perform \n";
+    }
+
+    public function setId ($id)
+    {
+        $this->_id = $id;
+    }
+
+    public function setResourcesUsage ($usage)
+    {
+        $this->_usage = $usage;
+    }
 }
